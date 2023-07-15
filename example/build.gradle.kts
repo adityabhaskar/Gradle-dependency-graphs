da3f7c6bf85bbb0b1112b79932a24d0f33fa2925
Aditya Bhaskar <adityabhaskar@users.noreply.github.com>
2023-07-15 12:59:34 +0100

    Add all optional properties with default values to extensions dsl in build.gradle.kts

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -7,5 +7,15 @@ dependencyGraphConfig {
 
     tag.set("Something else")
 
+    // Optional
     repoRootUrl.set("https://github.com/adityabhaskar/Project-Dependency-Graph/")
+
+    // Optional
+    graphFileName.set("dependencyGraph.md")
+
+    // Optional
+    graphDirection.set(net.c306.dependencygraph.plugin.Direction.LeftToRight)
+
+    // Optional
+    showLegend.set(net.c306.dependencygraph.plugin.ShowLegend.OnlyInRootGraph)
 }
