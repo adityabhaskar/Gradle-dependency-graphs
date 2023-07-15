a71a3518b91667cbe564ad29b2f831a8d3e30baf
Aditya Bhaskar <adityabhaskar@users.noreply.github.com>
2023-07-15 12:00:46 +0100

    Create and provide base url for clickable links `drawDependencies` so we can link the graphs correctly.
    + If no repo url is configured, don't add links
    + Add links to graphs, not to modules
    + Move functions to `CreateGraph.kt`

--- plugin-build/plugin/src/main/kotlin/net/c306/dependencygraph/plugin/DependencyGraphPlugin.kt
+++ plugin-build/plugin/src/main/kotlin/net/c306/dependencygraph/plugin/DependencyGraphPlugin.kt
@@ -22,7 +22,7 @@ abstract class DependencyGraphPlugin : Plugin<Project> {
             it.mainBranchName.set(extension.mainBranchName)
             it.repoRootUrl.set(extension.repoRootUrl)
             it.graphDirection.set(extension.graphDirection)
-            it.graphDetails.set(createGraph(project.rootProject))
+            it.graphDetails.set(parseDependencyGraph(project.rootProject))
         }
     }
 }
