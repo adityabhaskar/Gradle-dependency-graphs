009709cbddc9c9047e39435e049210dbea322e12
Aditya Bhaskar <adityabhaskar@users.noreply.github.com>
2023-07-20 08:26:05 +0100

    Formatting

--- plugin-build/plugin/build.gradle.kts
+++ plugin-build/plugin/build.gradle.kts
@@ -33,15 +33,17 @@ gradlePlugin {
             version = property("VERSION").toString()
             description = property("DESCRIPTION").toString()
             displayName = property("DISPLAY_NAME").toString()
-            tags.set(listOf(
-                "visualisation",
-                "graph",
-                "dependency",
-                "android",
-                "module",
-                "mermaid",
-                "github",
-            ))
+            tags.set(
+                listOf(
+                    "visualisation",
+                    "graph",
+                    "dependency",
+                    "android",
+                    "module",
+                    "mermaid",
+                    "github",
+                ),
+            )
         }
     }
 }
