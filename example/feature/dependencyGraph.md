37617a68658fe60282f4196ba0afdeb598211248
Aditya Bhaskar <adityabhaskar@users.noreply.github.com>
2023-07-21 10:01:06 +0100

    Remove ;

--- example/feature/dependencyGraph.md
+++ example/feature/dependencyGraph.md
@@ -4,9 +4,9 @@ graph LR;
 
 %% Styling for module nodes by type
 classDef rootNode stroke-width:4px;
-classDef mppNode fill:#ffd2b3;color:#333333;
-classDef andNode fill:#baffc9;color:#333333;
-classDef javaNode fill:#ffb3ba;color:#333333;
+classDef mppNode fill:#ffd2b3,color:#333333;
+classDef andNode fill:#baffc9,color:#333333;
+classDef javaNode fill:#ffb3ba,color:#333333;
 
 %% Modules
 subgraph  
--- plugin-build/plugin/src/main/kotlin/io/github/adityabhaskar/dependencygraph/plugin/core/DrawGraph.kt
+++ plugin-build/plugin/src/main/kotlin/io/github/adityabhaskar/dependencygraph/plugin/core/DrawGraph.kt
@@ -51,9 +51,9 @@ internal fun drawDependencyGraph(
 
     %% Styling for module nodes by type
     classDef rootNode stroke-width:4px;
-    classDef mppNode fill:#ffd2b3;color:#333333;
-    classDef andNode fill:#baffc9;color:#333333;
-    classDef javaNode fill:#ffb3ba;color:#333333;
+    classDef mppNode fill:#ffd2b3,color:#333333;
+    classDef andNode fill:#baffc9,color:#333333;
+    classDef javaNode fill:#ffb3ba,color:#333333;
     $legendText
     %% Modules
 
